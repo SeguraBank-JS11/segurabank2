e749d1bf94ea635d09ec1048561ea8fd732080b7
vinii-pimentel <vinii_2302@hotmail.com>
2026-02-03 13:35:35 -0300

    Fix dos conflitos da Models de  Categoria e Produto

--- src/models/Categoria.ts
+++ src/models/Categoria.ts
@@ -1,18 +1,8 @@
 import type Produto from "./Produto";
 
 export default interface Categoria {
-<<<<<<< HEAD
-  id: number;
-  nome: string;
-  usuario: string;
-  foto: string;
-  senha: string;
-  produto?: Produto[] | null;
-}
-=======
   id: number
   nome: string
   descricao: string
   produto?: Produto[] | null
 }
->>>>>>> dev
